bafc606ad957105a981da537f82f856823dcd2c6
nguyensinhnam <nguyensinhnam@gmail.com>
2018-06-29 14:32:41 +0700

    commit lan hai

--- hoibai/javascript/main.js
+++ hoibai/javascript/main.js
@@ -197,6 +197,16 @@
         });
     };
 
+    var countDown = function() {
+        var before = '<div class="square"><div class="numb">',
+            text = '</div><div class="text">';
+            if ($().countdown) {
+                $(".countdown").countdown('2018/12/22', function(event) {
+                  $(this).html(event.strftime(before + '%D' + text + 'Days</div></div>' + before + '%H' + text + 'Hours</div></div>' + before + '%M' + text + 'Minutes</div></div>' + before + '%S' + text + 'Seconds</div>'));
+                });
+            }      
+    };
+
     var removePreloader = function() { 
         $(window).load(function() { 
 
@@ -220,8 +230,8 @@
         goTop();
         topSearch();
         tabs();        
+        countdown();
         removePreloader();
-
    	});
 
 })(jQuery);
